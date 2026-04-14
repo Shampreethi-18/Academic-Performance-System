0202bbe351435e618f71d1c3cb6ed98203ee4d33
Sivanesan S <sivanesan.s.2024.it@rajalakshmi.edu.in>
2026-04-14 09:48:24 +0000

    Fixed Alternate Program module #19

--- alternate prog.java
+++ alternate prog.java
@@ -1,35 +1,15 @@
-import java.util.Scanner;
+public class AlternateProgram {
 
-public class AlternateProgramSuggester {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        double cgpa;
-        int arrears;
-
-        System.out.print("Enter student CGPA: ");
-        cgpa = sc.nextDouble();
-
-        System.out.print("Enter number of arrears: ");
-        arrears = sc.nextInt();
-
-        System.out.println("\nSuggestion:");
+    public static String suggest(double cgpa, int arrears) {
 
         if (cgpa >= 6.0 && arrears == 0) {
-            System.out.println("You can continue in your current program.");
+            return "Continue in current program";
         } 
         else if ((cgpa >= 5.0 && cgpa < 6.0) || (arrears > 0 && arrears <= 2)) {
-            System.out.println("Suggested Options:");
-            System.out.println("- Switch to a related program with lower difficulty");
-            System.out.println("- Consider academic support programs or tutoring");
+            return "Switch to related program or take academic support";
         } 
         else {
-            System.out.println("Suggested Options:");
-            System.out.println("- Diploma courses");
-            System.out.println("- Vocational training programs");
-            System.out.println("- Skill-based certifications (e.g., IT, Design, etc.)");
+            return "Consider diploma or vocational courses";
         }
-
-        sc.close();
     }
 }
