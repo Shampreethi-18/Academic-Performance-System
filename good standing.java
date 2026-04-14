dcdc472bf18047243404f4ca72a3ea28bed68801
Sivanesan S <sivanesan.s.2024.it@rajalakshmi.edu.in>
2026-04-14 09:54:35 +0000

    Fixed Good Standing module #17

--- good standing.java
+++ good standing.java
@@ -1,24 +1,6 @@
-import java.util.Scanner;
+public class GoodStanding {
 
-public class GoodStandingChecker {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        double cgpa;
-        int arrears;
-
-        System.out.print("Enter student CGPA: ");
-        cgpa = sc.nextDouble();
-
-        System.out.print("Enter number of arrears: ");
-        arrears = sc.nextInt();
-
-        if (cgpa >= 6.0 && arrears == 0) {
-            System.out.println("Status: Good Standing");
-        } else {
-            System.out.println("Status: Not in Good Standing");
-        }
-
-        sc.close();
+    public static boolean isGood(double cgpa, int arrears) {
+        return cgpa >= 6.0 && arrears == 0;
     }
-}
+}}
